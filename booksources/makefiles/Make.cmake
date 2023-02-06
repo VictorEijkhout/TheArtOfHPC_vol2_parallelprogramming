548853ffca303b378b89f8b0688d4f9a28926d4a
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-02-06 10:39:14 -0600

    makefile fix

--- booksources/makefiles/Make.cmake
+++ booksources/makefiles/Make.cmake
@@ -19,25 +19,39 @@ cmake :
 	@if [ -z "${PROGRAM}" ] ; then \
 	    echo "Please specify PROGRAM" && exit 1 \
 	 ; fi \
-	 && cmakelistsdir=`pwd`/.. \
-	 && echo "Start cmake in: `pwd`" \
+	 \
 	 && installdir=`pwd`/exe && builddir=`pwd`/build \
 	 && rm -rf $$installdir && mkdir -p $$installdir \
 	 && rm -rf $$builddir && mkdir -p $$builddir \
+	 \
+	 && if [ -z "${CXX_COMPILER}" ] ; then \
+	        echo "Please set CXX_COMPILER" && exit 1 ; fi \
+	 && if [ -z "${LMOD_FAMILY_MPI}" ] ; then \
+	        echo "Please use Lmod to load MPI" && exit 1 ; fi \
+	 && export CXX=${CXX_COMPILER} \
+	 && export CC=${C_COMPILER} \
+	 && export language=$$( echo ${LANGUAGE} | tr A-Z a-z ) \
+	 && export cmakeliststxt=CMakeLists.${MODE}.$${language}.txt \
+	 \
+	 && echo "Start cmake in: `pwd`" \
 	 && echo " .. building program : ${PROGRAM}" \
 	 && echo " .. cmake in   : `pwd`" \
-	 && echo " .. with code  : $$cmakelistsdir" \
 	 && echo " .. to prefix  : $$installdir" \
 	 && echo " .. using build: $$builddir" \
-	 && cp ../CMakeLists.txt . \
+	 && echo " .. cc         : $${CC}" \
+	 && echo " .. cxx        : $${CXX}" \
+	 && echo " .. script: $$cmakeliststxt" \
+	 \
+	 && cp ../../../makefiles/$$cmakeliststxt ./CMakeLists.txt \
 	 && cmake \
-	        -S $$cmakelistsdir -B $$builddir \
+	        -B $$builddir \
 	        -D PROJECT_NAME=${PROGRAM} \
+	        -D MPI_NAME=${LMOD_FAMILY_MPI} \
 	        -D CMAKE_INSTALL_PREFIX=$$installdir \
 	        -D CMAKE_VERBOSE_MAKEFILE=ON \
 	 && echo && echo " .. cmaking done" && echo \
 	 && ( cd $$builddir && make V=1 && make install ) \
-	 && echo && echo " .. installation:" && ls $$installdir && echo
+	 && echo && echo " .. installation in $$installdir:" && ls $$installdir && echo
 cmake_multi :
 	@cmakelistsdir=`pwd` \
 	 && echo "Start cmake in: `pwd`" \
