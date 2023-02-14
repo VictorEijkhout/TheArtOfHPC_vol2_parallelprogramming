a1eb9ab67d1b7f194070a65e3e3089f3510b962d
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-02-14 11:35:59 -0600

    new exercises

---
+++ exercises/exercises-mpi-c/setdiffprobe.c
@@ -0,0 +1,148 @@
+/****************************************************************
+ ****
+ **** This program file is part of the book 
+ **** `Parallel programming for Science and Engineering'
+ **** by Victor Eijkhout, eijkhout@tacc.utexas.edu
+ ****
+ **** copyright Victor Eijkhout 2012-2021
+ ****
+ **** MPI Exercise to illustrate pipelining
+ ****
+ ****************************************************************/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "mpi.h"
+
+#ifndef N
+#define N 10
+#endif
+
+/*
+ * Compare local data `mydata' against incoming `otherdata'
+ * Set local data to negative if it occurs in the incoming
+ */
+void setdiff( int *mydata,int *otherdata,int n ) {
+  for ( int i_my=0; i_my<n; i_my++) {
+    int my = mydata[i_my];
+    for ( int i_ot=0; i_ot<n; i_ot++ ) {
+      int ot = otherdata[i_ot];
+      if (my==ot) { mydata[i_my] = -1; break; }
+    }
+  }
+}
+
+/*
+ * Print the filter array by first gathering it
+ * Note: this is not a scalable thing to do.
+ */
+void print_distarray( int *data,int n,MPI_Comm comm ,char *txt,int sort) {
+  int nprocs,procno;
+  MPI_Comm_size(comm,&nprocs);
+  MPI_Comm_rank(comm,&procno);
+  int *global_data = procno==0 ? (int*) malloc( nprocs*n*sizeof(int) ) : NULL;
+  MPI_Gather( data,n,MPI_INT,global_data,n,MPI_INT,0,comm);
+  if (procno==0) {
+    if (sort) {
+      for (int top=nprocs*n-1; top>=0; top--) {
+	for (int s=0; s<top; s++) {
+	  if (global_data[s+1]<global_data[s]) {
+	    int t = global_data[s+1];
+	    global_data[s+1] = global_data[s];
+	    global_data[s] = t;
+	  }
+	}
+      }
+    }
+    printf("%s",txt);
+    for (int i=0; i<nprocs*n; i++) printf(" %3d",global_data[i]);
+    printf("\n");
+    free(global_data);
+  }
+}
+
+int main(int argc,char **argv) {
+
+  MPI_Init(&argc,&argv);
+  MPI_Comm comm = MPI_COMM_WORLD;
+
+  int nprocs, procno;
+  MPI_Comm_size(comm,&nprocs);
+  MPI_Comm_rank(comm,&procno);
+
+  // Initialize the random number generator
+  srand(procno);
+
+  // Set `sendto' and `recvfrom'
+  int sendto   = ( procno+1 ) % nprocs;
+  int recvfrom = ( procno-1+nprocs ) % nprocs;
+
+  /*
+   * Generate random local and other data
+   */
+  int mydata[N], filter[N],result[N];
+  for (int i=0; i<N; i++) {
+    mydata[i] = rand() % (2*N*nprocs);
+    result[i] = mydata[i];
+    filter[i] = rand() % (2*N*nprocs);
+  }
+
+  print_distarray(mydata,N,comm,"Starting : ",0);
+  print_distarray(filter,N,comm,"Filtering: ",1);
+
+  /*
+   * Exercise:
+   * -- compute the global set difference 
+   *    by passing the filter to every other process, non-blocking
+   */
+
+  // 1. apply the filter locally
+  setdiff( result,filter,N );
+
+  // 2. use non-blocking sends to send filter to everyone else
+  MPI_Request sendreqs[nprocs];
+  for (int target=0; target<nprocs; target++) {
+    // Send my filter to everyone else
+/**** your code here ****/
+  }
+
+  /*
+   * 3. receive everyone else's filter
+   *   note that there is only one buffer
+   *   what does that imply for the kind of receive?
+   */
+  int incoming[N];
+  // Receive all other filters
+  for (int stage=0; stage<nprocs-1; stage++) {
+/**** your code here ****/
+    setdiff( result,incoming,N );
+  }
+  MPI_Waitall(nprocs,sendreqs,MPI_STATUSES_IGNORE);
+  
+  print_distarray(result,N,comm,"Ending   : ",0);
+
+  /*
+   * Correctness check:
+   * `error' will be:
+   * - the lowest process number where an error occured, or
+   * - `nprocs' if no error.
+   */
+  double max_of_errors = 0;
+  int
+    error = max_of_errors > 1.e-12 ? procno : nprocs,
+    errors=-1;
+  MPI_Allreduce(&error,&errors,1,MPI_INT,MPI_MIN,comm);
+  if (procno==0) {
+    if (errors==nprocs) 
+      printf("Finished; all results correct\n");
+    else
+      printf("First error occurred on proc %d\n",errors);
+  }
+
+  MPI_Finalize();
+  return 0;
+}
---
+++ exercises/exercises-mpi-p/scangather.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+################################################################
+################################################################
+####
+#### This program file is part of the book and course
+####   Parallel Computing for Science and Engineering
+#### by Victor Eijkhout, copyright 2013-2023
+####
+#### scangatherp.py : scanning and gathering
+####
+################################################################
+################################################################
+
+from mpi4py import MPI
+import math
+import numpy as np
+import random
+import sys
+
+comm = MPI.COMM_WORLD
+
+nprocs = comm.Get_size()
+procno = comm.Get_rank()
+
+##
+## How many elements do I have locally?
+#### Allocate space.
+##
+random.seed(procno)
+my_number_of_elements = np.zeros(1,dtype=np.intc)
+my_number_of_elements[0] = int( random.random() * nprocs )
+my_first_index = np.zeros(1,dtype=np.intc)
+my_first_index[0] = 0
+
+## 
+##  Exercise:
+##  - determine the index of your first element.
+##    Which routine do you use here?
+##
+
+#### your code here ####
+##answersnippet end
+#comm.YourScan(my_number_of_elements,my_first_index,MPI.SUM)
+print( f"Proc {procno} has {my_number_of_elements[0]} elements, range [ {my_first_index[0]},{my_first_index[0]+my_number_of_elements[0]} )" )
+
+## 
+##  Create a local array of size `my_number_of_elements'
+##  Fill in this local array
+##
+
+total_number_of_elements = np.zeros(1,dtype=np.intc)
+my_elements = np.zeros(my_number_of_elements,dtype=np.intc)
+##answersnippet scangatherp2
+for i_element in range(my_number_of_elements[0]):
+    my_elements[i_element] = my_first_index+i_element
+
+comm.Reduce( my_number_of_elements,total_number_of_elements,MPI.SUM,0)
+##answersnippet end
+if procno==0:
+    print( f"Total number of elements: {total_number_of_elements}" )
+
+## 
+##  Gather all local arrays into one global
+##
+## how many elements from each process?
+size_buffer = np.zeros(nprocs,dtype=np.intc)
+##answersnippet scangatherp3
+comm.Gather(
+#### your code here ####
+   );
+## where are they going to go in the big buffer?
+displ_buffer = np.zeros( nprocs,dtype=np.intc )
+comm.Gather(
+#### your code here ####
+   );
+## now create the big buffer
+gather_buffer = np.zeros(total_number_of_elements,dtype=np.intc)
+
+## 
+##  Use Gatherv to collect the small buffers into a big one
+##
+comm.Gatherv(
+#### your code here ####
+   )
+##answersnippet end
+
+## 
+##  Print the gathered material
+##
+if procno==0:
+  print(f"Gathered: {gather_buffer}")
+  # for i_element in range(total_number_of_elements[0]):
+  #   print(" {gather_buffer[i_element]}")
+
+
