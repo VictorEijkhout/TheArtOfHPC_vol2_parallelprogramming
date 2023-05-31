465f083005d3b065897d8539295653b397743ccf
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2023-05-31 14:56:24 -0500

    fortran omp reduction

--- booksources/makefiles/Make.cmake
+++ booksources/makefiles/Make.cmake
@@ -13,12 +13,13 @@
 info ::
 	@echo && echo "================ Cmake rules:"
 	@echo "make cmake PROGRAM=..."
-	@echo "make cmake_multi"
-.PHONY: cmake cmake_multi
+	@echo "make cmake_apps"
+.PHONY: cmake cmake_apps
 cmake :
 	@if [ -z "${PROGRAM}" ] ; then \
 	    echo "Please specify PROGRAM" && exit 1 \
 	 ; fi \
+	 && program=${PROGRAM} && program=$${program%%.*} \
 	 \
 	 && installdir=`pwd`/exe && builddir=`pwd`/build \
 	 && rm -rf $$installdir && mkdir -p $$installdir \
@@ -34,7 +35,7 @@ cmake :
 	 && export cmakeliststxt=CMakeLists.${MODE}.$${language}.txt \
 	 \
 	 && echo "Start cmake in: `pwd`" \
-	 && echo " .. building program : ${PROGRAM}" \
+	 && echo " .. building program : $${program}" \
 	 && echo " .. cmake in   : `pwd`" \
 	 && echo " .. to prefix  : $$installdir" \
 	 && echo " .. using build: $$builddir" \
@@ -52,47 +53,22 @@ cmake :
 	 && cp ${CMAKEFILESDIR}/$$cmakeliststxt ./CMakeLists.txt \
 	 && cmake \
 	        -B $$builddir \
-	        -D PROJECT_NAME=${PROGRAM} \
-	        -D MPI_NAME=${LMOD_FAMILY_MPI} \
+	        -D PROJECT_NAME=$${program} \
+	        $$( if [ "${MODE}" = "mpi" ] ; then \
+	              echo "-D MPI_NAME=${LMOD_FAMILY_MPI}" \
+	            ; fi ) \
 	        -D CMAKE_INSTALL_PREFIX=$$installdir \
 	        -D CMAKE_VERBOSE_MAKEFILE=ON \
 	 && echo && echo " .. cmaking done" && echo \
 	 && ( cd $$builddir && make V=1 && make install ) \
 	 && echo && echo " .. installation in $$installdir:" && ls $$installdir && echo
-cmake_multi :
-	@cmakelistsdir=`pwd` \
-	 && echo "Start cmake in: `pwd`" \
-	 && installdir=`pwd`/exe && builddir=`pwd`/build \
-	 && rm -f CMakeLists.{mpi,omp}.txt \
-	 && extension=$$( make --no-print-directory extension ) \
-	     && cp ../../../Makefiles/CMakeLists.${MODE}.$${extension}.txt CMakeLists.txt \
-	 && rm -rf $$installdir && mkdir -p $$installdir \
-	 && rm -rf $$builddir && mkdir -p $$builddir \
-	 && echo " .. building program list:" \
-	 && if [ ! -z ${SINGLEFILE_APPS} ] ; then \
-	        programs="${SINGLEFILE_APPS}" \
-	    ; elif [ ! -z "${APPS}" ] ; then \
-	        programs="${APPS}" \
-	    ; else \
-	        echo "Variable APPS not set" && exit 1 \
-	    ; fi \
-	 && programs=$$( echo $$programs | tr ' ' ';' ) \
-	 && echo "    $$programs" && echo \
-	 && echo " .. cmake in   : `pwd`" \
-	 && echo " .. with code  : $$cmakelistsdir" \
-	 && echo " .. to prefix  : $$installdir" \
-	 && echo " .. using build: $$builddir" \
-	 && cmake \
-	        -S $$cmakelistsdir -B $$builddir \
-	        -D CMAKE_CXX_COMPILER=`which clang++` \
-	        -D CMAKE_C_COMPILER=`which clang` \
-	        -D CMAKE_Fortran_COMPILER=/opt/local/bin/gfortran \
-	        -D CMAKE_INSTALL_PREFIX=$$installdir \
-	        -D CMAKE_VERBOSE_MAKEFILE=ON \
-	        -D programs=$$programs \
-	 && echo && echo " .. cmaking done" && echo \
-	 && ( cd $$builddir && make V=1 && make install ) \
-	 && echo && echo " .. installation:" && ls $$installdir && echo
+cmake_apps :
+	@( \
+	  for a in $$( make --no-print-directory listapps | grep -v Warning ) ; do \
+	      make --no-print-directory cmake PROGRAM=$$a \
+	  ; done \
+	 ) 2>&1 | tee cmake.log \
+	 && echo "See cmake.log"
 
 localclean ::
 	@rm -rf build exe CMake*
