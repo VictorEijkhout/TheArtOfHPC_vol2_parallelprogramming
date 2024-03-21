7b49e1976b4e6c9e52d1a30312f0a2e3042b39e7
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2024-03-21 15:01:34 -0500

    update

--- exercises/exercises-omp-c/taskfactor.c
+++ exercises/exercises-omp-c/taskfactor.c
@@ -13,14 +13,13 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-// did you know that C99 has bool/true/false?
 #include <stdbool.h>
 #include <math.h>
 #include <omp.h>
 
 bool try_as_factor( long int i,long int bignum ) {
   if (bignum%i==0) {
-    printf("found %d\n",i);	  
+    printf("found %ld\n",i);
     return true;
   }
   return false;
@@ -28,20 +27,20 @@ bool try_as_factor( long int i,long int bignum ) {
 
 int main() {
 
-  long int bignum = 179 * 181 *	191 * 193 * 197 * 199 * 211 * 223 * 227 * 229,
-    //2000000111,
-    maxfactor = sqrt(bignum)+1, // 42000;
+  long int bignum = ( (long int) (179) ) * 181 * 191 * 193 * 197 * 199 * 211 * 223,
+    maxfactor = sqrt(bignum)+1;
   bool found = 0;
 
 /**** your code here ****/
-  for (long int i=2; i<bignum; i++) {
+    for (long int f=2; f<maxfactor; f++) {
 /**** your code here ****/
-    try_as_factor(i,bignum);
-    /*
-     * Can you make the whole process stopp
-     * when any factor has been found?
-     */
+	try_as_factor(f,bignum);
+      /*
+       * Can you make the whole process stopp
+       * when any factor has been found?
+       */
 /**** your code here ****/
+    }
 /**** your code here ****/
   }
 
